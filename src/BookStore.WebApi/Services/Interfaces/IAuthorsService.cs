5d1c06cebbc593d03604f73645d62747ba01a41e
csflorido <122863222+csflorido@users.noreply.github.com>
2024-08-17 16:08:42 +0800

    add IAuthorsService

---
+++ src/BookStore.WebApi/Services/Interfaces/IAuthorsService.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.WebApi.Services
+{
+    public interface IAuthorsService
+    {
+        IQueryable<Models.Author> Fetch();
+        Task<Models.Author> GetById(Guid id);
+        Task<Models.Author> Insert(Models.AuthorInsertDto author);
+        Task<Models.Author> Replace(Guid id, Models.AuthorInsertDto author);
+        Task<Models.Author> Update(Guid id, Models.AuthorUpdateDto author);
+        Task Delete(Guid id);
+    }
+}
